b08d3accc1f31976672e8e387b1e17fd0d613b53
zolar <zolars@outlook.com>
2017-09-19 18:45:01 +0800

    2017-09-19 18:45:00

--- yuan/代码片段/shopping/test_cart.cpp
+++
@@ -1,8 +0,0 @@
-#include "cart.h"
-#include <stdio.h>
-
-int main() {
-  char user_id[30] = "guke";
-  cart_main(user_id);
-  return 0;
-}
--- yuan/代码片段/董泽元/test_search_for_market.cpp
+++
@@ -1,6 +0,0 @@
-#include "search_for_market.h"
-int main() {
-  char user_id[30] = "guanli";
-  search_market_begin(user_id);
-  return 0;
-}
